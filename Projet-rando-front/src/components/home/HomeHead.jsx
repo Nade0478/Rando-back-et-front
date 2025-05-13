9c4545e5fd0fc24629249a07b0591757b60da955
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-05-13 10:37:25 +0200

    Scroll rajouter dans home+css

--- Projet-rando-front/src/components/home/HomeHead.jsx
+++ Projet-rando-front/src/components/home/HomeHead.jsx
@@ -8,26 +8,32 @@ const HomeHead = () => {
     <section className="homeHead">
       <div className="image-container">
         <div className="overlay">
-          <div className="container text-container">
-            <h1 className="titreHome">Bienvenue sur la page d'accueil de Rando-Ouest</h1>
-            <div className="content-wrapper">
-              <div className="text-box">
-                <p>
-                  Rando-Ouest est votre guide ultime pour découvrir les sentiers de
-                  randonnée pédestre les plus beaux et variés du Grand Ouest de la
-                  France. Que vous soyez novice ou expérimenté, notre site vous
-                  propose des itinéraires adaptés, des conseils pratiques et une
-                  communauté de passionnés.
-                </p>
-              </div>
+          <div className="page-container">
+            <div className="container text-container">
+              <h1 className="titreHome">
+                Bienvenue sur la page d'accueil de Rando-Ouest
+              </h1>
+              <div className="content-wrapper">
+                <div className="text-box">
+                  <p>
+                    Rando-Ouest est votre guide ultime pour découvrir les
+                    sentiers de randonnée pédestre les plus beaux et variés du
+                    Grand Ouest de la France. Que vous soyez novice ou
+                    expérimenté, notre site vous propose des itinéraires
+                    adaptés, des conseils pratiques et une communauté de
+                    passionnés.
+                  </p>
+                </div>
 
-              <div className="text-box">
-                <p>
-                  Commencez votre exploration dès aujourd'hui avec Rando-Ouest. Que
-                  vous soyez en quête de randonnées en famille, de défis sportifs ou
-                  de moments de détente en pleine nature, notre site a tout ce qu'il
-                  vous faut pour organiser votre prochaine sortie.
-                </p>
+                <div className="text-box">
+                  <p>
+                    Commencez votre exploration dès aujourd'hui avec
+                    Rando-Ouest. Que vous soyez en quête de randonnées en
+                    famille, de défis sportifs ou de moments de détente en
+                    pleine nature, notre site a tout ce qu'il vous faut pour
+                    organiser votre prochaine sortie.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
--- Projet-rando-front/src/components/home/HomeNewArticle.jsx
+++ Projet-rando-front/src/components/home/HomeNewArticle.jsx
@@ -1,66 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./HomeNew.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
 
 const HomeNewArticle = () => {
-  const [items, setItems] = React.useState([]);
-  // const items = [
-  //   {
-  //     id: 1,
-  //     title: "Expertise locale",
-  //     description: "Nos itinéraires sont conçus pour vous faire découvrir les trésors cachés.",
-  //     image: `${process.env.REACT_APP_API_URL}/storage/app/public/public/uploads/alpes-mancelle72_1742481007.jpg`,
-  //   },
-  //   {
-  //     id: 54,
-  //     title: "Descriptions des parcours",
-  //     description: "Chaque itinéraire est accompagné de descriptions détaillées.",
-  //     image: `${process.env.REACT_APP_API_URL}/storage/public/uploads/Site-historique.jpg`,
-  //   },
-  //   {
-  //     id: 53,
-  //     title: "Cartes interactives",
-  //     description: "Accédez à des cartes détaillées et interactives pour planifier vos randonnées.",
-  //     image: `${process.env.REACT_APP_API_URL}/storage/public/uploads/carte-randonnée.png`,
-  //   },
-  // ];
+  const [items, setItems] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     displayArticleHome(); // Appel de la fonction pour récupérer les articles au montage du composant
   }, []);
 
   const displayArticleHome = async () => {
     try {
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/article-home`);
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/article-home`
+      );
       setItems(response.data);
-      console.log(items);
     } catch (error) {
-      console.error("Erreur lors de la récupération des lieux :", error);
+      console.error("Erreur lors de la récupération des articles :", error);
     }
   };
 
   return (
     <section className="homeNewArticle">
-      <h3 className="text-center">ARTICLES</h3>
-      <div className="items">
-        {items.map((item) => (
-          <div key={item.id} className="item">
-            <div className="image-container">
-              <Link to={`/article/show/${item.id}`}>
-                <img src={item.image} alt={item.title_article} />
+      <div className="page-container">
+        <h3 className="text-center">ARTICLES</h3>
+
+        {/* Scroll horizontal */}
+        <div className="items scroll-container">
+          {items.map((item) => (
+            <div key={item.id} className="item">
+              <div className="image-container">
+                <Link to={`/article/show/${item.id}`}>
+                  <img src={item.image} alt={item.title_article} />
+                </Link>
+              </div>
+              <h3 className="item-title">{item.title_article}</h3>
+              <p>{item.content_article}</p>
+              <Link to={`/article/show/${item.id}`} className="btn custom-btn">
+                Découvrir
               </Link>
             </div>
-            <h3 className="item-title">{item.title_article}</h3>
-            <p>{item.content_article}</p>
-            <Link to={`/article/show/${item.id}`} className="btn custom-btn">
-              Découvrir
-            </Link>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
